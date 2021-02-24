0fcb602428ab520f9e69b1468eca3e01a28f82e2
Tristan <tristanshawnjohnson@gmail.com>
2021-02-24 15:46:27 -0800

    Created an html file and a css file. Also added a use static line to our server to serve that file

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
+
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
 
+app.use(express.static(path.join(__dirname, '../public/index.html')));
 
 app.listen(port, () => {
-  console.log(`Server is listening on ${port}`)
-})
+  console.log(`Server is listening on ${port}`);
+});
