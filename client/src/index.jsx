4b9f149b146cceb1f44a3524acfe3e89b7beae0f
Darian Chan <darian.s.chan@gmail.com>
2021-02-26 11:36:27 -0800

    installed sass

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import App from './App.jsx'
 
 ReactDOM.render(<App />, document.getElementById('app'));
