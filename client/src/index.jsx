aa8819fdea72ebc9b9141e34147f257aeb85ec71
Ross Russell <ross.russell@protonmail.com>
2021-02-25 16:16:42 -0800

    React/redux is installed, a bit of skeleton code, still need to read full docs.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// React redux stuff.
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from '@reduxjs/toolkit';
+// All reducers combined in rootReducer.
+// import rootReducers from './reducers';
+// End redux stuff.
 import App from './App.jsx';
 
+// Create our first data store.
+// composeWithDevTools() enables debug through Redux Dev Tools
+// Passing our reducers, and a dummy state.
+// const store = createStore(
+//   rootReducers, { itemActions: { favorites: [] } }, composeWithDevTools(),
+// );
+// Set up our rootElement, we'll need this for Provider
+// which allows us to pass state through our entire app.
+const rootElement = document.getElementById('app');
 
-ReactDOM.render(<App />, document.getElementById('app'));
-
+// Easy redux guide https://programmingwithmosh.com/redux/getting-started-with-react-redux/
+// ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
+ReactDOM.render(<App />, rootElement);
