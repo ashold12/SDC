8c4920f83daabc41c902fef21b369e5b97d2c423
Ross Russell <ross.russell@protonmail.com>
2021-02-26 15:25:07 -0800

    Fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -20,15 +20,17 @@ module.exports = {
           presets: ['@babel/preset-env', '@babel/preset-react'],
         },
       },
-    ],
-    test: /\.s[ac]ss$/i,
-    use: [
-      // Creates `style` nodes from JS strings
-      'style-loader',
-      // Translates CSS into CommonJS
-      'css-loader',
-      // Compiles Sass to CSS
-      'sass-loader',
+      {
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+        test: /\.s[ac]ss$/i,
+      }
     ],
   },
 };
