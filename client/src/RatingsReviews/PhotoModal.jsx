dbee74789ed93ea67c66104f25ccd9e2f758cc1e
Ross Russell <ross.russell@protonmail.com>
2021-02-27 17:44:02 -0800

    checked off images, modals, recommends, responses are there.

---
+++ client/src/RatingsReviews/PhotoModal.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const RRModal = ({ displayModal, closeModal, image }) => {
+  // Pass in whether or not we're displaying the modal with display modal.
+  // closeModal is the function to change our display to false.
+  // image is the url of the modal image.
+
+  const modalClassName = displayModal ? 'rr-modal rr-display-block' : 'rr-modal rr-display-none';
+  return (
+    <div className={modalClassName}>
+      <section className="rr-modal-main">
+        <img alt="Product shown very large." className="rr-modal-image" src={image} />
+        <button type="button" onClick={closeModal}>
+          Close
+        </button>
+      </section>
+    </div>
+  );
+};
+
+export default RRModal;
--- client/src/RatingsReviews/ReviewTile.jsx
+++ client/src/RatingsReviews/ReviewTile.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
+import RRModal from './PhotoModal.jsx';
 import dummyReviews from './dummyReviews';
 
 class ReviewTile extends React.Component {
   constructor(props) {
     super(props);
     // Single test review testing block.
-    let [testReview] = dummyReviews.results;
+    let [testReview, testReview2] = dummyReviews.results;
     // Calculate star rating.
     if (testReview.rating > 0) {
       testReview.starRating = parseFloat((Math.round(testReview.rating * 4) / 4).toFixed(2));
     }
     this.state = {
       ...testReview,
+      showModal: false,
+      modalURL: '',
     };
     this.state.body =
       this.state.body +
-      'Some super long winded review that no one is ever going to read because it is stupid and who cares about all this bs seriously its like who cares there is so much other stuff to do and these reviews are useless';
+      'Some super long winded review that no one is ever going to read because it is stupid and who cares about all this bs seriously its like who cares there is so much other stuff to do and these reviews are useless and yet they continue to be written forever and ever and it just goes on and on so lets just make the best of the review and fill in this text and make it like super long.';
     this.showMoreReview = this.showMoreReview.bind(this);
+    this.showPhotoModal = this.showPhotoModal.bind(this);
+    this.hidePhotoModal = this.hidePhotoModal.bind(this);
   }
 
   getReviewSummary() {
@@ -68,6 +73,22 @@ class ReviewTile extends React.Component {
     return <div className="rr-body-text">{body}</div>;
   }
 
+  getPhotosHTML() {
+    const { photos } = this.state;
+    const photoList = photos.map((photo) => {
+      return (
+        <img
+          className="rr-product-thumbnail"
+          key={photo.id}
+          src={photo.url}
+          onClick={this.showPhotoModal}
+          alt="Product small."
+        />
+      );
+    });
+    return photoList;
+  }
+
   getFormattedDate() {
     const date = new Date(this.state.date);
     const months = [
@@ -92,9 +113,34 @@ class ReviewTile extends React.Component {
     return formattedDate;
   }
 
-  showMoreReview(e) {
+  showPhotoModal(e) {
+    this.setState({ showModal: true, modalURL: e.target.src });
+  }
+
+  hidePhotoModal() {
+    this.setState({ showModal: false, modalURL: '' });
+  }
+
+  showMoreReview() {
     const bodyText = document.getElementById(this.state.review_id);
     bodyText.innerText = this.state.body;
+  }
+
+  getRecommendedHTML() {
+    if (this.state.recommend) {
+      return <div className="rr-recommended">✓ I recommend this product.</div>;
+    }
+  }
+
+  getResponseHTML() {
+    if (this.state.response) {
+      return (
+        <div className="rr-staff-response">
+          Response from seller
+          <div>{this.state.response}</div>
+        </div>
+      );
+    }
   }
 
   hasRegisteredEmail() {
@@ -109,10 +155,11 @@ class ReviewTile extends React.Component {
     const { reviewer_name } = this.state;
     let checkmark = '';
     if (this.hasRegisteredEmail()) {
-      checkmark = '✓';
+      checkmark = '✓ Verified Purchaser';
     }
 
     const title = this.getReviewSummary();
+
     return (
       <div className="rr-review-tile-container">
         <div
@@ -127,6 +174,14 @@ class ReviewTile extends React.Component {
           <b>{title}</b>
         </div>
         <div className="rr-review-body">{this.getReviewBodyHTML()}</div>
+        {this.getRecommendedHTML()}
+        <div className="rr-photos-div">{this.getPhotosHTML()}</div>
+        {this.getResponseHTML()}
+        <RRModal
+          displayModal={this.state.showModal}
+          closeModal={this.hidePhotoModal}
+          image={this.state.modalURL}
+        />
       </div>
     );
   }
