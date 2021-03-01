b1fb452e32bd1d3cde02276cf9034f4a222f4fcf
Tristan <tristanshawnjohnson@gmail.com>
2021-03-01 10:25:29 -0800

    Have started my component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './style.scss';
 import RatingsReviews from './RatingsReviews/RatingsReviews.jsx';
+import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -11,6 +12,7 @@ class App extends React.Component {
     return (
       <div>
         react is up and running
+        <QuestionsAndAnswers />
         <RatingsReviews />
       </div>
     );
