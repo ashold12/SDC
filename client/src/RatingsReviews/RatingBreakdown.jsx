40d5c3f594242596fa013d90e5e295d835b3d35a
Ross Russell <ross.russell@protonmail.com>
2021-03-01 14:35:52 -0800

    Added bars for reviews.

--- client/src/RatingsReviews/RatingBreakdown.jsx
+++ client/src/RatingsReviews/RatingBreakdown.jsx
@@ -20,6 +20,30 @@ class RatingBreakdown extends React.Component {
       });
   }
 
+  getReviewBar(indexNumber, starNumber) {
+    let numberOfVotes = this.state.ratings[starNumber];
+    let percentage;
+    debugger;
+    if (!this.state.ratings[starNumber]) {
+      percentage = 0;
+      numberOfVotes = 0;
+    } else {
+      percentage = (this.state.ratings[starNumber] / this.state.totalNumberOfReviews) * 100;
+    }
+    const barStyling = {
+      width: `${percentage}%`,
+      backgroundColor: '#4CAF50',
+    };
+    return (
+      <div className="rr-review-bar-star-count">
+        # Stars {indexNumber} With {numberOfVotes} votes.
+        <div className="rr-review-bar-container">
+          <div className="rr-review-bar" style={barStyling} id={`rr-bar-${starNumber}`}></div>
+        </div>
+      </div>
+    );
+  }
+
   setInitialState(data) {
     // Iterate through the object keys, pull their values and multiply the key times value.\
     // Star calculating formula
@@ -31,10 +55,14 @@ class RatingBreakdown extends React.Component {
       totalNumberOfRatings += parseInt(data.ratings[key], 10);
     });
     const toNearestDecimal = (totalRatingScore / totalNumberOfRatings).toFixed(1);
+    // Calculate the recommended amount and the number of reviewers.
+    const usersRecommendedPercentage =
+      (parseInt(data.recommended.true, 10) / totalNumberOfRatings) * 100;
     this.setState({
       ...data,
       starRating: toNearestDecimal,
       totalNumberOfReviews: totalNumberOfRatings,
+      usersRecommendedPercentage: usersRecommendedPercentage,
     });
   }
 
@@ -43,10 +71,16 @@ class RatingBreakdown extends React.Component {
       return <div />;
     }
     const { starRating, totalNumberOfReviews } = this.state;
+    // Map ratings.
+    let ratingBars = [];
+    for (let i = 1; i <= 5; i++) {
+      let bar = this.getReviewBar(i, i);
+      ratingBars.push(bar);
+    }
     return (
       <div className="rr-breakdown">
-        Average Rating:
-        {starRating}
+        Average Rating: {starRating} {this.state.usersRecommendedPercentage}% of reviews recommend
+        this product.
         <div
           className="Stars"
           style={{ '--rating': starRating }}
@@ -54,6 +88,7 @@ class RatingBreakdown extends React.Component {
         />
         Number of Reviews:
         {totalNumberOfReviews}
+        <div className="rr-review-bar-container">{ratingBars}</div>
       </div>
     );
   }
--- client/src/RatingsReviews/RatingsReviews.jsx
+++ client/src/RatingsReviews/RatingsReviews.jsx
@@ -19,14 +19,6 @@ class RatingsReviews extends React.Component {
         Ratings & Reviews
         <div className="rr-rating-big">
           <RatingBreakdown productId={this.state.product_id} />
-          <div className="rr-rating-breakdown">
-            Rating Breakdown
-            <div className="rr-rating">5 divs with rating breakdowns</div>
-            <div className="rr-rating">5 divs with rating breakdowns</div>
-            <div className="rr-rating">5 divs with rating breakdowns</div>
-            <div className="rr-rating">5 divs with rating breakdowns</div>
-            <div className="rr-rating">5 divs with rating breakdowns</div>
-          </div>
         </div>
         <ReviewTile item={product_id} />
       </div>
