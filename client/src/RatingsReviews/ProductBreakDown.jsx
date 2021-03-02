1f90d3b28c50140f6fc8e64a15eed2ebe3144bae
Ross Russell <ross.russell@protonmail.com>
2021-03-02 14:25:21 -0800

    Product breakdown is now loading into the page. reshuffled some loading in axios

--- client/src/RatingsReviews/ProductBreakDown.jsx
+++ client/src/RatingsReviews/ProductBreakDown.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
-class ProductBreakDown extends React.Component { 
+class ProductBreakDown extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      productId: 17762,
+      chars: props.characteristics,
+    };
+    console.log(props.characteristics);
   }
-  
+
+  render() {
+    return (
+      <div className="rr-product-breakdown">
+        breakdown is here
+      </div>
+    );
+  }
 }
+
+export default ProductBreakDown;
--- client/src/RatingsReviews/RatingsReviews.jsx
+++ client/src/RatingsReviews/RatingsReviews.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import RatingBreakdown from './RatingBreakdown.jsx';
 import ReviewTile from './ReviewTile.jsx';
+import ProductBreakDown from './ProductBreakDown.jsx';
 import axios from 'axios';
 
 class RatingsReviews extends React.Component {
@@ -8,24 +9,34 @@ class RatingsReviews extends React.Component {
     super(props);
     this.tempReview = 17762;
     this.state = {
-      loaded: false,
+      loadedReviews: false,
+      loadedMeta: false,
       product_id: this.tempReview,
       filters: [],
     };
   }
 
   componentDidMount() {
-    axios.get(`/api/reviews/?product_id=${this.tempReview}`).then((data) => {
-      this.setState({
-        loaded: true,
-        product_id: this.tempReview,
-        reviews: data.data.results
-      });
-    });
+    axios
+      .get(`/api/reviews/?product_id=${this.tempReview}`)
+      .then((data) => {
+        this.setState({
+          loadedReviews: true,
+          product_id: this.tempReview,
+          reviews: data.data.results
+        });
+      })
+      .catch((e) => { console.log(e) });
+    axios
+      .get(`/api/reviews/meta?product_id=${this.tempReview}`)
+      .then((data) => {
+        this.setState({ meta: data.data, loadedMeta: true });
+      })
+      .catch((e) => { console.log(e); });
   }
 
   render() {
-    if (this.state.loaded === false) {
+    if (this.state.loadedReviews === false || this.state.loadedMeta === false) {
       return <div />;
     }
     //Get two reviews.
@@ -44,6 +55,9 @@ class RatingsReviews extends React.Component {
         </div>
         <div className="rr-rating-breakdown">
           <RatingBreakdown productId={product_id} filters={filters} />
+        </div>
+        <div className='rr-product-breakdown-container'>
+          <ProductBreakDown characteristics={this.state.meta} />
         </div>
       </div>
     );
