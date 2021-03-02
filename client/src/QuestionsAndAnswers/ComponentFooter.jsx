66a5a484c45b12ecc6d10d22d8b932b543c49fdd
Tristan <tristanshawnjohnson@gmail.com>
2021-03-01 17:52:47 -0800

    Refactored a few things

--- client/src/QuestionsAndAnswers/ComponentFooter.jsx
+++ client/src/QuestionsAndAnswers/ComponentFooter.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const ComponentFooter = function (props) {
   return (
     <div>
-      <button>Load More Answers</button>
       <div>
         <button>More Answered Questions</button>
         <button>Add A Question</button>
