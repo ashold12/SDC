28564da370bfc17b7df8435f27f0b4238819ebcd
Ross Russell <ross.russell@protonmail.com>
2021-03-02 13:46:26 -0800

    Setup module to load two revies at a time.

---
+++ client/src/RatingsReviews/ProductBreakDown.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class ProductBreakDown extends React.Component { 
+  constructor(props) {
+    super(props)
+  }
+  
+}
--- client/src/RatingsReviews/RatingsReviews.jsx
+++ client/src/RatingsReviews/RatingsReviews.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import RatingBreakdown from './RatingBreakdown.jsx';
 import ReviewTile from './ReviewTile.jsx';
+import axios from 'axios';
 import dummyReviews from './dummyReviews';
 
 class RatingsReviews extends React.Component {
   constructor(props) {
     super(props);
+    this.tempReview = 17762;
     this.state = {
-      product_id: dummyReviews.product,
-      rating: dummyReviews.results[0].rating,
+      loaded: false,
+      product_id: props.reviewId,
       filters: [],
     };
   }
 
+  componentDidMount() {
+    axios.get(`/api/reviews/?product_id=${this.tempReview}`).then((data) => {
+      this.setState({
+        loaded: true,
+        product_id: this.tempReview,
+        reviews: data.data.results
+      });
+    });
+  }
+
   render() {
+    if (this.state.loaded === false) {
+      return <div />;
+    }
+    //Get two reviews.
+    const tiles = [];
+    for (let i = 0; i < 2; i += 1) {
+      tiles.push(<ReviewTile item={product_id} review={this.state.reviews[i]} />);
+    }
     const { product_id, filters } = this.state;
     return (
       <div className="rr-parent">
@@ -23,7 +43,7 @@ class RatingsReviews extends React.Component {
             <RatingBreakdown productId={product_id} filters={filters} />
           </div>
         </div>
-        <ReviewTile item={product_id} />
+        {tiles}
       </div>
     );
   }
