3088b1476629be54ae234af6d914f6460bbf694f
Darian Chan <darian.s.chan@gmail.com>
2021-03-02 10:48:07 -0800

    got api to render prices and name

--- client/src/Overview/ProductOverview.jsx
+++ client/src/Overview/ProductOverview.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const ProductOverview = (props) => {
   return (
-    <div>
-      {/* <div>description goes here</div> */}
+    <div className='o-productOverview'>
+      <div>blah blah blah blah blah blah blah blah</div>
+      <div className='o-productOverviewText'>description goes here</div>
     </div>
   );
 };
