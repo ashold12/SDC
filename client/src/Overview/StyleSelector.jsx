cafedab829a48ea4c0577b101e9bd434ecd3b014
Darian Chan <darian.s.chan@gmail.com>
2021-03-04 11:52:44 -0800

    refactored everything and cleaned it up now

--- client/src/Overview/StyleSelector.jsx
+++ client/src/Overview/StyleSelector.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import {BiCheckCircle} from 'react-icons/bi'
 
 const StyleSelector = ({selectedStyle, selectedProductStyles, selectStyleThumbnail}) => {
-  // need to use an onclick handler
-    // should this.setState({selectedStyle: eventTarget}) when clicked
+
+  // name is dynamic based on how long it takes state to be set
   let name = "loading";
+
   if (selectedStyle) {
     name = selectedStyle.name
   }
@@ -14,12 +15,15 @@ const StyleSelector = ({selectedStyle, selectedProductStyles, selectStyleThumbna
       Style > {name}
       <div className="o-selectStyleThumbnail">
         {selectedProductStyles.map((style) => {
+          // put checkmark on the thumbnail if clicked
           if (selectedStyle.style_id === style.style_id) {
             return (<div key={style.style_id}>
               <BiCheckCircle/>
               <img key={style.style_id} className="o-items" src={style.photos[0].thumbnail_url} onClick={()=>{selectStyleThumbnail(style)}}></img>
             </div>)
-          } else {
+          }
+          // if the thumbnail is not selected, don't include the checkmark
+          else {
             return <img key={style.style_id} className="o-items" src={style.photos[0].thumbnail_url} onClick={()=>{selectStyleThumbnail(style)}}></img>
           }
       })}
