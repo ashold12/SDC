857dbf84a11fea2ae8f880aaf22b46da994b2ba9
Ross Russell <ross.russell@protonmail.com>
2021-03-04 17:44:27 -0800

    fixed axios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,28 +5,22 @@ import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers.jsx';
 import Overview from './Overview/Overview.jsx';
 import RelatedItemsAndComparison from './RelatedItemsAndComparison/RelatedItemsAndComparison.jsx';
 import axios from 'axios';
-
 class App extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       allProducts: [],
       selectedProduct: null,
-
       questions: {},
     };
-
     this.getAllProducts = this.getAllProducts.bind(this);
     this.getProduct = this.getProduct.bind(this);
     this.getQuestions = this.getQuestions.bind(this);
   }
-
   componentDidMount() {
     this.getAllProducts();
     this.getProduct();
   }
-
   getAllProducts() {
     axios
       .get('api/products?count=*')
@@ -40,27 +34,21 @@ class App extends React.Component {
         console.log(error);
       });
   }
-
   getProduct() {
-
-    axios.get('api/products/17762')
-      .then((product) => {
-        this.setState({
-          selectedProduct: product.data
-        }, () => {
-          this.getQuestions();
-        });
-
     axios.get('api/products/17762').then((product) => {
-      this.setState({
-        selectedProduct: product.data,
-
-      });
+      this.setState(
+        {
+          selectedProduct: product.data,
+        },
+        () => {
+          this.getQuestions();
+        }
+      );
     });
   }
-
   getQuestions() {
-    axios.get(`api/qa/questions/?product_id=${this.state.selectedProduct.id}`)
+    axios
+      .get(`api/qa/questions/?product_id=${this.state.selectedProduct.id}`)
       .then((questions) => {
         this.setState({
           questions: questions.data,
@@ -70,7 +58,6 @@ class App extends React.Component {
         console.error(error);
       });
   }
-
   render() {
     const { selectedProduct } = this.state;
     return (
@@ -79,19 +66,15 @@ class App extends React.Component {
         {/*need to pass in what item we're on here*/}
         <Overview selectedProduct={selectedProduct} />
         <RelatedItemsAndComparison allProducts={this.state.allProducts} />
-
-        {this.state.questions.results && <QuestionsAndAnswers
-          selectedProduct={this.state.selectedProduct}
-          selectedProductsQuestions={this.state.questions}
-
-        />}
+        {this.state.questions.results && (
+          <QuestionsAndAnswers
+            selectedProduct={this.state.selectedProduct}
+            selectedProductsQuestions={this.state.questions}
+          />
+        )}
         <RatingsReviews />
-
-        <QuestionsAndAnswers />
-        <RatingsReviews productData={selectedProduct} />
       </div>
     );
   }
 }
-
 export default App;
