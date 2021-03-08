c11769ccd54516c5cbe40387ea22c673273e4e8a
Tristan <tristanshawnjohnson@gmail.com>
2021-03-07 18:32:17 -0800

    Adding functionality to show images of answers

--- client/src/QuestionsAndAnswers/AnswerModal/AnswerModalImages.jsx
+++ client/src/QuestionsAndAnswers/AnswerModal/AnswerModalImages.jsx
@@ -14,15 +14,17 @@ const AnswerModalImages = function ({ photos, onChange, imageClose }) {
     );
   }
   return (
+    <React.Fragment>
     <div className="qa-answer-modal-griditem13">
       {photos.map((photo) => (
         <div className="qa-answer-modal-photos" key={photos.indexOf(photo)}>
           <input type="click" className="qa-answer-modal-remove-photo" onClick={imageClose} placeholder="Remove Image"/>
           <img className="qa-answer-thumbnails" src={photo} alt="Image you just uploaded" />
-        </div>
+          </div>
       ))}
-      <input type="file" accept="image/*" placeholder="Add Image" onChange={onChange} />
     </div>
+      <input className="qa-answer-imageUploadButton" type="file" accept="image/*" placeholder="Add Image" onChange={onChange} />
+    </React.Fragment>
   );
 };
 
