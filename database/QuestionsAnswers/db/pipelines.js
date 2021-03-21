8e5a6e296bf397a81d2b3f91d264925940c3d21c
AlexShold <ashold12@gmail.com>
2021-03-21 18:42:53 -0500

    answers limited to two top values

--- database/QuestionsAnswers/db/pipelines.js
+++ database/QuestionsAnswers/db/pipelines.js
@@ -17,11 +17,14 @@ const makeQuestionPipeline = (id, start, end) => [
           },
         },
         { $unwind: '$answers' },
+        { $match: { 'answers.reported': { $lt: 1 } } },
+        { $sort: { 'answers.helpful': -1 } },
+        { $limit: 2 },
         {
           $project: {
             _id: 0,
-            k: { $toString: '$answers._id' },
-            v: '$$ROOT.answers',
+            k: { $toString: '$answers._id' }, // k
+            v: '$$ROOT.answers', // v
           },
         },
       ],
@@ -61,3 +64,67 @@ const makeQuestionPipeline = (id, start, end) => [
 module.exports = {
   makeQuestionPipeline,
 };
+
+/*
+db.prodquests.aggregate([
+  { $match: { _id: 1 } },
+  { $unwind: '$questions' },
+  { $match: { 'questions.reported': { $lt: 1 } } },
+  { $sort: { 'questions.helpful': -1 } },
+  { $group: { _id: '$_id', questions: { $push: '$questions' } } },
+  { $project: { _id: 1, questions: { $slice: ['$questions', 0, 100] } } },
+  { $unwind: '$questions' },
+  {
+    $lookup: {
+      from: 'groupansphotos',
+      let: { question_id: '$questions._id' },
+      pipeline: [
+        {
+          $match: {
+            $expr: { $eq: ['$_id', '$$question_id'] },
+          },
+        },
+        { $unwind: '$answers' },
+        { $match: { 'answers.reported': { $lt: 1 } } },
+        { $sort: { 'answers.helpful': -1 } },
+        {
+          $project: {
+            _id: 0,
+            k: { $toString: '$answers._id' },
+            v: '$$ROOT.answers',
+          },
+        },
+      ],
+      as: 'answers',
+    },
+  },
+  {
+    $match: {
+      $expr: {
+        $gt: [{ $size: '$answers' }, 0],
+      },
+    },
+  },
+  {
+    $addFields: {
+      answers: { $arrayToObject: '$answers' },
+    },
+  },
+  {
+    $group: {
+      _id: '$_id',
+      questions: {
+        $push: {
+          question_id: '$questions._id',
+          question_body: '$questions.body',
+          question_date: '$questions.date_written',
+          asker_name: '$questions.asker_name',
+          question_helpfulness: '$questions.helpful',
+          reported: '$questions.reported',
+          answers: '$answers',
+        },
+      },
+    },
+  },
+])
+*/
