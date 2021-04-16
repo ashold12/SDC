fc9cc92d6b08293f44ba6a08ec01ed885520b3d5
AlexShold <ashold12@gmail.com>
2021-04-16 12:24:05 -0500

    pulled specific section

--- client/src/RelatedItemsAndComparison/relatedOutfitQuery.js
+++
@@ -1,9 +0,0 @@
-// import axios from 'axios';
-
-// module.exports.getRelatedItems = (productID) => {
-//   axios
-//     .get(`/api/products/${productID}/related`)
-//     .then((related) => this.setState({ relatedProducts: related.data }))
-//     .then(() => console.log(this.state.relatedProducts))
-//     .catch((err) => console.log(err));
-// };
--- server/server.test.js
+++
@@ -1,8 +0,0 @@
-// const server = require('./index.js');
-
-describe('server', () => {
-  test('Basic test', () => {
-    const cat = 'cat';
-    expect(typeof cat).toBe('string');
-  });
-});
